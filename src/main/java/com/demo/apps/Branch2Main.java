83953b11c49d3d3e01d297e147dd8e0d341de300
ravi224466 <ravi224466@gmail.com>
2023-12-13 20:22:11 +0530

    added branch1 new method

--- src/main/java/com/demo/apps/Branch2Main.java
+++ src/main/java/com/demo/apps/Branch2Main.java
@@ -4,4 +4,8 @@ public class Branch2Main {
     public static void main(String[] args) {
         System.out.println("Hello...........!");
     }
+	
+	public void branch1Method1(){
+		System.out.println("Method added from branch-1!");
+	}
 }
